f71e0a49e634bc99a46394f303d7a2eef86c6477
19PA1A0512 <60499456+19PA1A0512@users.noreply.github.com>
2021-07-11 17:57:29 +0530

    Create contactform.php

---
+++ contactform.php
@@ -0,0 +1,37 @@
+<?php
+ include ('connect.php');
+if(isset($_POST['name']) && isset($_POST['password']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['msg'])){
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+       $name=$_POST["name"];
+       $password=$_POST["password"];
+       $email=$_POST["email"];
+       $phone=$_POST["phone"];
+       $msg=$_POST["msg"];
+       $qry1="INSERT INTO data(name,password,email,phone,msg) VALUES ('$name','$password','$email','$phone','$msg')";
+       $res1=mysqli_query($conn,$qry1);
+    }
+    else{
+       
+        echo "failed";
+    }
+    mysqli_close($conn);
+}   
+?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>login</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+     <form id="one" method="POST" action="">
+     <label><b>Contact form</b></label><br><br>
+     username:<input type="text"  id="userid" name="name" required><br><br>
+     password:<input type="password" id="pass" name="password" required><br><br>
+     Email id:<input type="email"  id="mailid" name="email"><br><br>
+     phone no:<input type="tel" id="phone" name="phone" pattern="[0-9]{10}" required><br><br>
+     Message :<textarea  id="msg" name="msg" required></textarea><br><br>
+     <input type="submit" value="Submit" id="submit">
+    </form>
+</body>
+</html>
